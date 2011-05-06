ceb2b2aa7b0e8016a2fbe4a72e3065124e18cdf6
Roman Masek <romario333@gmail.com>
2011-05-06 19:11:57 +0200

    Initial Commit

---
+++ DiskUsagePgTests/DiskUsagePgTests.h
@@ -0,0 +1,17 @@
+//
+//  DiskUsagePgTests.h
+//  DiskUsagePgTests
+//
+//  Created by Roman Masek on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DiskUsagePgTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ DiskUsagePgTests/DiskUsagePgTests.m
@@ -0,0 +1,33 @@
+//
+//  DiskUsagePgTests.m
+//  DiskUsagePgTests
+//
+//  Created by Roman Masek on 5/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DiskUsagePgTests.h"
+
+
+@implementation DiskUsagePgTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DiskUsagePgTests");
+}
+
+@end
